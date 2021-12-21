0585acf60a798fd714875f04091f52300c041bba
Valerian Saliou <valerian@valeriansaliou.name>
2021-12-21 12:37:23 +0100

    Format message dates OK

--- src/messaging/components/message/message.js
+++ src/messaging/components/message/message.js
@@ -1,6 +1,9 @@
 // CONSTANTS
 
+const INVALID_FALLBACK = "(?)"
 const LINE_BREAK_REGEX = /\n/g;
+const SPACE_REGEX = /\s/g;
+const DATE_FORMAT_LOCATION = "en-US";
 
 // COMPONENTS
 
@@ -8,25 +11,52 @@ function Message(message) {
   return {
     // --> DATA <--
 
-    date: message.date,
     user: message.user,
+    date: null,
     lines: null,
 
     // --> METHODS <--
 
     mounted() {
-      this.lines = message.content.map((content) => {
-        // Generate message HTML (based on type)
+      // Generate message lines HTML (based on type)
+      this.lines = this.generateLines(message.content)
+
+      // Generate message date text
+      this.date = this.generateDate(message.date)
+    },
+
+    generateLines(contents) {
+      return contents.map((content) => {
         switch (content.type) {
           case "text": {
+            // Generate lines
             return content.text.replace(LINE_BREAK_REGEX, "<br>")
           }
 
           default: {
-            return "(?)"
+            // Type is invalid
+            return INVALID_FALLBACK
           }
         }
       })
+    },
+
+    generateDate(dateString) {
+      let date = new Date(dateString)
+
+      // Date is valid?
+      if (isNaN(date.getTime()) === false) {
+        let timeString = date.toLocaleString(DATE_FORMAT_LOCATION, {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true
+        })
+
+        return timeString.toLowerCase().replace(SPACE_REGEX, "")
+      }
+
+      // Date is invalid
+      return INVALID_FALLBACK
     }
   }
 }
