7e7fd8e8c5c53a033a308bf1013bd28705bf4b7a
Valerian Saliou <valerian@valeriansaliou.name>
2022-06-18 16:31:24 +0200

    style: add source code headers

--- src/messaging/components/message/message.js
+++ src/messaging/components/message/message.js
@@ -1,3 +1,10 @@
+/*
+ * prose-core-views
+ *
+ * Copyright: 2022, Valerian Saliou <valerian@valeriansaliou.name>
+ * License: Mozilla Public License v2.0 (MPL v2.0)
+ */
+
 // CONSTANTS
 
 const INVALID_FALLBACK = "(?)"
