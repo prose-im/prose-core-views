82ef29e432c08d8b88b4dbfdcebc3d652bb9a2b4
Valerian Saliou <valerian@valeriansaliou.name>
2022-06-19 20:47:21 +0200

    chore: add lint via action

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+
+  env: {
+    browser: true
+  },
+
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false
+  },
+
+  extends: ["prettier"],
+
+  plugins: [],
+  rules: {}
+};
